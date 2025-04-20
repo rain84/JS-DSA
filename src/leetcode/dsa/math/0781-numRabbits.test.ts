2a10d7258eb735de2ffc901d81b0958a90719a7f
rain84 <rainy_sky@mail.ru>
2025-04-21 02:53:38 +0300

    feat(LeetCode): ✨ add numRabbits (lc. 0781)

---
+++ src/leetcode/dsa/math/0781-numRabbits.test.ts
@@ -0,0 +1,17 @@
+import { numRabbits } from './0781-numRabbits'
+
+it.each(
+  //  prettier-ignore
+  [
+    [
+      [1,1,2],
+      5
+    ],
+    [
+      [10,10,10],
+      11
+    ]
+  ]
+)('should work %#', (input, output) => {
+  expect(numRabbits(input)).toBe(output)
+})
---
+++ src/leetcode/dsa/math/0781-numRabbits.ts
@@ -0,0 +1,20 @@
+/**
+ * {@link https://leetcode.com/problems/rabbits-in-forest | 781. Rabbits in Forest}
+ *
+ * Topics: Array | Hash Table | Math | Greedy
+ */
+export function numRabbits(answers: number[]): number {
+  const cnt: Record<number, number> = {}
+  let res = 0
+
+  for (const x of answers) {
+    if (cnt[x]) {
+      cnt[x]--
+    } else {
+      cnt[x] = x
+      res += x + 1
+    }
+  }
+
+  return res
+}
