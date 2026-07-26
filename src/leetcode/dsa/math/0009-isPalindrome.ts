5e42bc98815342a5ea0352ab4bb6b79fc719eae7
rain84 <rainy_sky@mail.ru>
2026-07-26 15:51:48 +0300

    feat(LeetCode): add isPalindrome (lc. 0009)

---
+++ src/leetcode/dsa/math/0009-isPalindrome.ts
@@ -0,0 +1,27 @@
+/**
+ * {@link https://leetcode.com/problems/palindrome-number/ | 9. Palindrome Number
+}
+ *
+ * Topics: Math
+ */
+
+export function isPalindrome(x: number): boolean {
+  const str1 = String(x)
+  const str2 = [...str1].reverse().join('').toString()
+
+  return str1 === str2
+};
+
+export function isPalindrome2(x: number): boolean {
+  const str = String(x)
+  const n = str.length
+  const end = Math.floor(n / 2)
+
+  for (let i = 0; i < end; i++) {
+    if (str[i] !== str[n - i - 1]) {
+      return false
+    }
+  }
+
+  return true
+};
