a9c3873a585f5f900cb47055376b1edb720774bf
rain84 <rainy_sky@mail.ru>
2026-08-01 02:39:08 +0300

    chore(leetcode): remove duplicated two-sum solution

--- src/leetcode/0000-0099/0001-two-sum.test.ts
+++
@@ -1,8 +0,0 @@
-import { twoSum } from './0001-two-sum'
-
-describe('Leetcode: Two-sum', () => {
-	it('should work', () => {
-		expect(twoSum([2, 7, 11, 15], 9)).toEqual([0, 1])
-		expect(twoSum([3, 2, 4], 6)).toEqual([1, 2])
-	})
-})
