9781f7271dd24672aed872ab9d58789f742feca5
rain84 <rainy_sky@mail.ru>
2026-08-24 21:07:51 +0300

    feat(LeetCode): add removeElement with two implementations + tests (lc. 0027)

---
+++ src/leetcode/0000-0099/0027-removeElement.test.ts
@@ -0,0 +1,46 @@
+import { _removeElement, removeElement } from './0027-removeElement'
+
+it.each(
+	// prettier-ignore
+	[
+		[[3, 2, 2, 3], 3, 2, [2, 2]],
+		[[0, 1, 2, 2, 3, 0, 4, 2], 2, 5, [0, 1, 4, 0, 3]],
+		[[], 1, 0, []],
+		[[1], 1, 0, []],
+		[[1], 2, 1, [1]],
+		[[2, 2, 2], 2, 0, []],
+		[[1, 2, 3, 4], 5, 4, [1, 2, 3, 4]],
+		[[4, 5], 4, 1, [5]],
+		[[3, 3], 3, 0, []],
+		[[0, 0, 0, 0], 0, 0, []],
+	],
+)('should remove %i from %p -> length %i', (nums, val, expectedLen, expectedPrefix) => {
+	const arr = [...nums]
+	const len = removeElement(arr, val)
+	expect(len).toBe(expectedLen)
+	expect(arr.slice(0, len).sort()).toEqual(expectedPrefix.sort())
+})
+
+it.each(
+	// prettier-ignore
+	[
+		[[3, 2, 2, 3], 3, 2, [2, 2]],
+		[[0, 1, 2, 2, 3, 0, 4, 2], 2, 5, [0, 1, 4, 0, 3]],
+		[[], 1, 0, []],
+		[[1], 1, 0, []],
+		[[1], 2, 1, [1]],
+		[[2, 2, 2], 2, 0, []],
+		[[1, 2, 3, 4], 5, 4, [1, 2, 3, 4]],
+		[[4, 5], 4, 1, [5]],
+		[[3, 3], 3, 0, []],
+		[[0, 0, 0, 0], 0, 0, []],
+	],
+)(
+	'_removeElement: should remove %i from %p -> length %i',
+	(nums, val, expectedLen, expectedPrefix) => {
+		const arr = [...nums]
+		const len = _removeElement(arr, val)
+		expect(len).toBe(expectedLen)
+		expect(arr.slice(0, len).sort()).toEqual(expectedPrefix.sort())
+	},
+)
---
+++ src/leetcode/0000-0099/0027-removeElement.ts
@@ -0,0 +1,33 @@
+/**
+ * 27. Remove Element
+ * {@link https://leetcode.com/problems/remove-element/ | Link}
+ *
+ * Topics: Array | Two Pointers
+ *
+ */
+export function removeElement(nums: number[], val: number): number {
+	let i = 0
+
+	for (const x of nums) {
+		if (x !== val) {
+			nums[i++] = x
+		}
+	}
+
+	return i
+}
+
+export function _removeElement(nums: number[], val: number): number {
+	let [l, r] = [0, nums.length - 1]
+
+	while (l <= r) {
+		if (nums[l] === val) {
+			;[nums[l], nums[r]] = [nums[r], nums[l]]
+			r--
+		} else {
+			l++
+		}
+	}
+
+	return l
+}
