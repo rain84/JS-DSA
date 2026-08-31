c23e3f9cf0cfd650855e47cfbd27d653d0b43d6b
rain84 <rainy_sky@mail.ru>
2026-08-31 11:32:52 +0300

    chore: remove unused removeDuplicates files

--- src/leetcode/removeDuplicates.test.ts
+++
@@ -1,8 +0,0 @@
-import { removeDuplicates } from './removeDuplicates'
-
-it('should work', () => {
-	const nums = [1, 1, 2]
-
-	expect(removeDuplicates(nums)).toBe(2)
-	expect(nums).toMatchObject([1, 2])
-})
