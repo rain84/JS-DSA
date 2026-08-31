8ef1241ead293a46d10500af939bb399ed2f8af1
rain84 <rainy_sky@mail.ru>
2026-08-31 20:05:14 +0300

    feat(LeetCode): add moveZeroes with tests (lc. 0283)

--- src/leetcode/0200-0299/0283-moveZeroes.test.ts
+++ src/leetcode/0200-0299/0283-moveZeroes.test.ts
@@ -1,13 +1,21 @@
 import { moveZeroes } from './0283-moveZeroes'
 
-it('should work 1', () => {
-	const data = [0, 1, 0, 3, 12]
-	moveZeroes(data)
-	expect(data).toMatchObject([1, 3, 12, 0, 0])
-})
-
-it('should work 2', () => {
-	const data = [0]
-	moveZeroes(data)
-	expect(data).toMatchObject([0])
+it.each(
+	// prettier-ignore
+	[
+		[[0, 1, 0, 3, 12], [1, 3, 12, 0, 0]],
+		[[0], [0]],
+		[[1, 2, 3], [1, 2, 3]],
+		[[0, 0, 0], [0, 0, 0]],
+		[[1, 0, 2, 0, 3], [1, 2, 3, 0, 0]],
+		[[4, 2, 4, 0, 0, 3, 0, 5, 1, 0], [4, 2, 4, 3, 5, 1, 0, 0, 0, 0]],
+		[[1, 0], [1, 0]],
+		[[0, 1], [1, 0]],
+		[[1, 0, 1], [1, 1, 0]],
+		[[], []],
+	],
+)('moveZeroes(%p) = %p', (input, expected) => {
+	const arr = [...input]
+	moveZeroes(arr)
+	expect(arr).toEqual(expected)
 })
--- src/leetcode/0200-0299/0283-moveZeroes.ts
+++ src/leetcode/0200-0299/0283-moveZeroes.ts
@@ -1,15 +1,23 @@
 /**
  * 283. Move Zeroes
  * {@link https://leetcode.com/problems/move-zeroes/ | Link}
+ *
+ * Topics: Array | Two Pointers
+ *
+ * Given an integer array nums, move all 0's to the end of it while maintaining
+ * the relative order of the non-zero elements.
+ * Note that you must do this in-place without making a copy of the array.
  */
 export function moveZeroes(nums: number[]): void {
-	const n = nums.length
-	for (let i = 0, j = 0; i < n; i++) {
-		if (nums[i] && !nums[j]) {
-			;[nums[i], nums[j]] = [nums[j], nums[i]]
-			j++
-		}
+	let i = 0
 
-		if (nums[j]) j++
+	for (const x of nums) {
+		if (x) {
+			nums[i++] = x
+		}
+	}
+
+	while (i < nums.length) {
+		nums[i++] = 0
 	}
 }
